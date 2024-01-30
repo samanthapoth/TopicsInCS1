508db4d713d5de48f79c4ce589c7ab5730a39587
samanthapoth <samantha-pothitakis@uiowa.edu>
2024-01-30 12:41:59 -0600

    first commit

---
+++ sam.py
@@ -0,0 +1,10 @@
+import math
+
+# Get user input
+num = int(input("Enter a number: "))
+
+# Calculate square root
+sqrt_num = math.sqrt(num)
+
+# Output the result
+print("The square root of", num, "is", sqrt_num)
