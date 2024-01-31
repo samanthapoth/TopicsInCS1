92443af4d509610c57a00cf3e1bea56af6682f51
samanthapoth <samantha-pothitakis@uiowa.edu>
2024-01-31 16:43:40 -0600

    fibTimePlot updated with comments

--- fibTimePlot.py
+++ fibTimePlot.py
@@ -4,10 +4,12 @@ fib(100)
 
 import matplotlib.pyplot as plt
 
+# swap the first two elements of the array so that the first point plotted is fib(0) and the second is fib(1)
 temp = times[0]
 times [0] = times[1]
 times[1] = temp
 
+#plot the times array
 plt.plot(times)
 plt.ylabel('Time in seconds')
 plt.xlabel('fib(x)')
